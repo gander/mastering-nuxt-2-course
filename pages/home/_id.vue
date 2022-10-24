fe470f1d64b97f4ba5e1ea5946d2d60241abca41
Adam Gąsowski <adam.gasowski@gander.pl>
2022-10-24 11:20:51 +0200

    Pages and Routing: How Does Routing in Nuxt Work?; Creating the Property Page

---
+++ pages/home/_id.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="home">
+    <div class="images">
+      <img v-for="image in home.images" :key="image" :src="image" alt="">
+    </div>
+    <div>{{ home.title }}</div>
+    <div><img src="/images/marker.svg" width="20" height="20" alt=""/> {{ home.location.address }}, {{ home.location.city }}, {{ home.location.state }}, {{ home.location.country }}</div>
+    <div><img src="/images/star.svg" width="20" height="20" alt=""/> {{ home.reviewValue }}</div>
+    <div>{{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds, {{ home.bathrooms }} bath</div>
+  </div>
+</template>
+
+<script>
+import homes from '~/data/homes.json';
+
+export default {
+  head() {
+    return {
+      title: this.home.title,
+    };
+  },
+  date() {
+    return {
+      home: {},
+    };
+  },
+  created() {
+    this.home = homes.find(home => home.objectID === this.$route.params.id);
+  },
+};
+</script>
+
+<style scoped>
+.images {
+  display: flex;
+}
+
+.images img {
+  width: 200px;
+  height: 150px;
+}
+</style>
