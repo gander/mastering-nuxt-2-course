a16f2fba1e46c81d5d1ccfad760fc2d1c14a34aa
Adam Gąsowski <adam.gasowski@gander.pl>
2022-10-24 14:42:38 +0200

    Working with Google Maps: Adding a Map To The Property Page

--- pages/home/_id.vue
+++ pages/home/_id.vue
@@ -7,6 +7,7 @@
     <div><img src="/images/marker.svg" width="20" height="20" alt=""/> {{ home.location.address }}, {{ home.location.city }}, {{ home.location.state }}, {{ home.location.country }}</div>
     <div><img src="/images/star.svg" width="20" height="20" alt=""/> {{ home.reviewValue }}</div>
     <div>{{ home.guests }} guests, {{ home.bedrooms }} rooms, {{ home.beds }} beds, {{ home.bathrooms }} bath</div>
+    <div class="map" ref="map"></div>
   </div>
 </template>
 
@@ -17,6 +18,11 @@ export default {
   head() {
     return {
       title: this.home.title,
+      script: [{
+        src: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCfJAXer4JGO46gfbyWOUh9ktICx8lMuLk',
+        hid: 'map',
+        defer: true,
+      }],
     };
   },
   date() {
@@ -26,6 +32,13 @@ export default {
   },
   created() {
     this.home = homes.find(home => home.objectID === this.$route.params.id);
+  },
+  mounted() {
+    const position = new window.google.maps.LatLng(this.home._geoloc.lat, this.home._geoloc.lng);
+    const mapOptions = {zoom: 18, center: position, disableDefaultUI: true, zoomControl: true};
+    const map = new window.google.maps.Map(this.$refs.map, mapOptions);
+    const marker = new window.google.maps.Marker({position});
+    marker.setMap(map);
   },
 };
 </script>
@@ -38,5 +51,10 @@ export default {
 .images img {
   width: 200px;
   height: 150px;
+}
+
+.map {
+  width: 800px;
+  height: 800px;
 }
 </style>
