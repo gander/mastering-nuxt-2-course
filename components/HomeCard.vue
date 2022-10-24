743b469b5d327f452e17feab3cc5325c92e8a925
Adam Gąsowski <adam.gasowski@gander.pl>
2022-10-24 10:40:44 +0200

    Pages and Routing: Creating Our Homepage

---
+++ components/HomeCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <img :src="home.images[0]" alt="">
+    <div>{{ home.title }}</div>
+    <div>{{ home.location.address }} {{ home.location.city }} {{ home.location.state }}</div>
+    <div>{{ home.guests }} guests</div>
+    <div>{{ home.pricePerNight }} / night</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    home: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.home {
+  float: left;
+  margin: 10px;
+}
+
+.home img {
+  width: 200px;
+  display: block;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <home-card v-for="home in homes" :key="home.objectID" :home="home"></home-card>
+  </div>
+</template>
+
+<script>
+import homes from '~/data/homes.json';
+
+export default {
+  data() {
+    return {
+      homes: homes.slice(0, 3),
+    };
+  },
+};
+</script>
