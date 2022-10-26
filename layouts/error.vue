39723d98ee6713ce87b1732a33d62bcd08f694cd
Adam Gąsowski <adam.gasowski@gander.pl>
2022-10-26 13:20:30 +0200

    Working with APIs: Customizing The Error Page

---
+++ layouts/error.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="main">
+    <img src="https://media.tenor.com/652b4Q5e-BMAAAAd/pat-mat-pat-a-mat.gif" alt="">
+    <div>Error!</div>
+    <div class="message">{{ error.message }} {{ error.statusCode }}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.main {
+  text-align: center;
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-items: center;
+  font: 40px sans-serif;
+}
+.message {
+  font-weight: 700;
+}
+</style>
